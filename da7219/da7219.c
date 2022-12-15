d29ea86206104f2b08a057348824f5a9d21a481e
CoolStar <coolstarorganization@gmail.com>
2022-12-15 01:17:43 -0800

    Allow disabling in device manager

--- da7219/da7219.c
+++ da7219/da7219.c
@@ -577,6 +577,14 @@ Da7219EvtDeviceAdd(
 		return status;
 	}
 
+	{
+		WDF_DEVICE_STATE deviceState;
+		WDF_DEVICE_STATE_INIT(&deviceState);
+
+		deviceState.NotDisableable = WdfFalse;
+		WdfDeviceSetDeviceState(device, &deviceState);
+	}
+
 	WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&queueConfig, WdfIoQueueDispatchParallel);
 
 	queueConfig.EvtIoInternalDeviceControl = Da7219EvtInternalDeviceControl;
